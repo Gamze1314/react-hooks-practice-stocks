efdc1203d01e0a7900c8969582f49dc12834ecc0
gamze <gamzekandara@gmail.com>
2024-02-18 04:53:17 -0500

    Update

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Stock({ stock, addOnPortfolio, onRemoveStock, portfolio }) {
+function Stock({ stock, addOnPortfolio, onRemoveStock}) {
   const { name, price } = stock;
 
   function handleClick(id) {
@@ -16,7 +16,7 @@ function Stock({ stock, addOnPortfolio, onRemoveStock, portfolio }) {
       <div onClick={() => handleClick(stock.id)} className="card">
         <div onClick={() => handleSell(stock)} className="card-body">
           <h5 className="card-title">{name}</h5>
-          <p className="card-text">{price}</p>
+          <p className="card-text">{stock.ticker} : {price}</p>
         </div>
       </div>
     </div>
